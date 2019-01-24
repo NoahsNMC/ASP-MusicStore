9d5c7c6e55cd441191ca4f1e9e032a910371f67b
NoahFlowa <osterhoutn@gmail.com>
2019-01-24 10:19:55 -0500

    Initial Commit

---
+++ MVCMusicStore/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string About()
+        {
+            //ViewBag.Message = "Your application description page.";
+
+            return "Noah Osterhout";
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View("About");
+        }
+
+        public string Browse(int id)
+        {
+            return "ID:"+id;
+        }
+    }
+}
